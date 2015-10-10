c731b95c15069aff631a737f7d444bab171e0ca6
Adrien <adrien.philippaux@gmail.com>
2015-10-10 12:46:57 +0200

    pan

--- Angry-Birds/src/Panneau.java
+++ Angry-Birds/src/Panneau.java
@@ -7,11 +7,8 @@ import javax.swing.JPanel;
 
 public class Panneau extends JPanel {
 
-	private Point p1 = new Point(20, 450);
-	private Point p2 = new Point(140, 60);
-	private Point p3 = new Point(800, 450);
 	private Obstacle o1 = new Obstacle(20, 450);
-	private Obstacle o2 = new Obstacle(140,60);
+	private Obstacle o2 = new Obstacle(140, 60);
 	private Obstacle o3 = new Obstacle(800, 450);
 	private int posX = 0;
 	private int posY = 0;
@@ -23,25 +20,22 @@ public class Panneau extends JPanel {
 		g.setColor(Color.red);
 		g.fillOval(posX, posY, 20, 20);
 		g.setColor(Color.orange);
-		g.fillArc(posX+19, posY - 15, 25, 50, 160, 30);
-		
+		g.fillArc(posX + 19, posY - 15, 25, 50, 160, 30);
+
 		for (int i = 0; i < pts.size(); i += 2) {
 			g.fillOval(pts.get(i).x + 2, pts.get(i).y + 3, 5, 5);
 		}
-		
+
 		pts.add(new Point(posX, posY));
-	
+
 		g.setColor(Color.blue);
 		o1.afficher(g);
 		o2.afficher(g);
 		o3.afficher(g);
-		
-		if(pts.size()>100){
+
+		if (pts.size() > 100) {
 			pts.removeAll(pts);
 		}
-		/*g.fillOval(p1.x, p1.y, 20, 20);
-		g.fillOval(p2.x, p2.y, 20, 20);
-		g.fillOval(p3.x, p3.y, 20, 20);*/
 
 	}
 

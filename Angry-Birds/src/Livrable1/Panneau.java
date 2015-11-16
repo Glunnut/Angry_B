d6524ebdd4e5ff9becdacca52b09097190a9bdd4
Affon <paulivanaffolabyn@hotmail.com>
2015-11-16 10:32:24 +0100

    Thomas est jaloux

--- Angry-Birds/src/Livrable1/Panneau.java
+++ Angry-Birds/src/Livrable1/Panneau.java
@@ -114,7 +114,6 @@ public class Panneau extends JPanel {
 		Obstacle.afficher(g);
 		oiseau.afficher(g);
 		oiseau.move(posX, posY);
-
 	}
 
 	//---------------------GETTERS--------------------------//
